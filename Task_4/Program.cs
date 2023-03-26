8fd2aa7b99b76e98d6dc536fb98cf3f94308d137
силивончик елена <e.silivonchik@sckkgit initgit config --global user.email e.silivonchik@sckk>
2023-03-27 02:15:28 +0300

    решена задача 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Находим max из трёх чисел
+Console.WriteLine("Введите три числа: ");
+string input1 = Console.ReadLine();
+string input2 = Console.ReadLine();
+string input3 = Console.ReadLine();
+int number1 = int.Parse(input1);
+int number2 = int.Parse(input2);
+int number3 = int.Parse(input3);
+int max = number1;
+
+if(number1 > max) max = number1;
+if(number2 > max) max = number2;
+if(number3 > max) max = number3;
+
+ Console.Write("max = ");
+ Console.WriteLine(max); 
