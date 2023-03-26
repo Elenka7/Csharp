8b723c9725724e9e49b32d9058a9be187f03cd55
силивончик елена <e.silivonchik@sckkgit initgit config --global user.email e.silivonchik@sckk>
2023-03-27 02:00:18 +0300

    решена задача 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Находим max из двух чисел
+Console.WriteLine("Введите два числа: ");
+string input1 = Console.ReadLine();
+string input2 = Console.ReadLine();
+int number1 = int.Parse(input1);
+int number2 = int.Parse(input2);
+int max = number1;
+
+if (number1 > number2)
+{
+    max = number1;
+}
+else
+{
+    max = number2;
+}
+ Console.Write("max = ");
+ Console.WriteLine(max); 
