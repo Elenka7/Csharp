483e2a2bc467a57355f18f27f8d397710e316f7b
силивончик елена <e.silivonchik@sckkgit initgit config --global user.email e.silivonchik@sckk>
2023-03-27 02:41:23 +0300

    решена задача 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,13 @@
+﻿// Определим четное ли число
+Console.WriteLine("Введите число: ");
+string m = Console.ReadLine();
+int n = int.Parse(m);
+
+if(n%2 == 0)
+{
+    Console.WriteLine("Это чётное число");
+}
+else
+{
+    Console.WriteLine("Это нечётное число");
+}
