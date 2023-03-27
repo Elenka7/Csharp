6f8cb69a8e597ead1839439532b000a3662ebe61
силивончик елена <e.silivonchik@sckkgit initgit config --global user.email e.silivonchik@sckk>
2023-03-27 03:01:01 +0300

    решена задача 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Найдём все чётные числа от 1 до N
+Console.WriteLine("Введите число: ");
+
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= n;)
+{
+   if(i%2 == 0)
+   {
+    Console.WriteLine(i);
+   }
+i++;
+}
